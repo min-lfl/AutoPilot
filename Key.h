53a38da010ad2e3653e8adee5e8113e4a3f42591
min-lfl <2775381313@qq.com>
2025-11-27 16:15:39 +0800

    优化了项目结构，把系统资源和硬件资源分开，增加了可移植性

--- Key.h
+++
@@ -1,7 +0,0 @@
-#ifndef __Key_H__
-#define __Key_H__
-
-void Scan_key();
-unsigned char switch0000();
-
-#endif
--- Timer0.h
+++
@@ -1,6 +0,0 @@
-#ifndef __Timer0_H__
-#define __Timer0_H__
-
-void Timer0_Init(void);
-
-#endif
